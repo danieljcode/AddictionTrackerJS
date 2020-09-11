1f874c844334eb830f20e86db941c43cab41d2b6
Daniel John <danieljohncode@tutanota.com>
2020-09-11 20:56:31 +0100

    Started working on the system for checking if there data inputted before saving it!

--- Javascript/startup.js
+++ Javascript/startup.js
@@ -2,15 +2,64 @@ const button = document.getElementById("submitButton");
 const container = document.getElementById("container");
 
 const nameInput = document.getElementById("nameIn");
-const nameInput = document.getElementById("addictionIn");
+const addictionInput = document.getElementById("addictionIn");
+
+function CheckForSavedData(){
+//This function checks if there is data saved in localStorage and redirects the user if there is
+
+    if(localStorage.getItem("USERNAME") == null || localStorage.getItem("USERNAME") == undefined){
+
+        //No data has been saved, keep the user on this start page
+
+    }else{
+
+        //Data has been saved... redirect the user away from this page to the main page
+
+    }
+
+
+}
+
+function CheckForBlanks(){
+
+    if(nameInput.value == ""){
+    
+
+
+    }
+
+    if(addictionIn.value == ""){
 
 
 
-button.addEventListener("submit", (e) =>{
+    }
+
+    if((!nameInput.value == "") && (!addictionIn.value == "")){
+
+        //Neither fields are empty... proceeding with the data saving
+
+    }
+}
+
+
+function SaveData(){
+
+    CheckForBlanks();
+
+    var name = nameInput.value;
+    console.log(name);
+
+    var addiction = addictionIn.value;
+    console.log(addiction);
+
+}
+
+
+button.addEventListener("click", (e) =>{
 
     e.preventDefault();
 
-
+    SaveData();
 
 
 })
