33adc2d81b7fc9d1196775d71a3e35eea46fbc1a
Daniel John <danieljohncode@tutanota.com>
2020-09-11 20:44:40 +0100

    Started linking in the Javascript!

--- Javascript/startup.js
+++ Javascript/startup.js
@@ -1,1 +1,16 @@
 const button = document.getElementById("submitButton");
+const container = document.getElementById("container");
+
+const nameInput = document.getElementById("nameIn");
+const nameInput = document.getElementById("addictionIn");
+
+
+
+button.addEventListener("submit", (e) =>{
+
+    e.preventDefault();
+
+
+
+
+})
