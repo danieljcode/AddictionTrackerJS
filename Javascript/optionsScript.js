4c162359a4115d3e64153fad5236de2317bb8d63
Daniel John <danieljohncode@tutanota.com>
2020-10-13 20:52:48 +0100

    Style and JS changes

--- Javascript/optionsScript.js
+++ Javascript/optionsScript.js
@@ -4,8 +4,11 @@ new TypeIt("#message", {
     cursor: false
   }).go();
 
+  const nextBtn = document.getElementById("nextBtn");
 const option1 = document.getElementById("substance");
 const option2 = document.getElementById("behavior");
+
+var option;
 
 option1.addEventListener("click", (event) =>{
 
@@ -13,11 +16,24 @@ option1.addEventListener("click", (event) =>{
   option1.classList.toggle("selected");
   option2.classList.remove("selected");
 
+  option = 1;
+
+  nextBtn.style.display="block"; //DISPLAYING THE NEXT BUTTON TO THE USER AFTER THEY SELECT AN OPTION
 })
 option2.addEventListener("click", (event) =>{
 
   console.log("User has selected behavioral addiction category");
   option2.classList.toggle("selected");
   option1.classList.remove("selected");
+  //Highlighting the box clicked and un-highlighting the other box
+
+  option = 2;
+
+  nextBtn.style.display="block"; //DISPLAYING THE NEXT BUTTON TO THE USER AFTER THEY SELECT AN OPTION
+})
+
+nextBtn.addEventListener("click", (event)=>{
+
+  localStorage.setItem("typeOfAddiction", option);
 
 })
