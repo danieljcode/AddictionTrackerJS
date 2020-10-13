fd46d28dfa10511f5f4fe9ec444240278572081a
Daniel John <danieljohncode@tutanota.com>
2020-10-13 21:54:31 +0100

    Style, JS changes

--- Javascript/optionsTwoScript.js
+++ Javascript/optionsTwoScript.js
@@ -1,18 +1,30 @@
 var optionSelected = localStorage.getItem("typeOfAddiction");
 var addictionType;
 
+const substanceSelect = document.getElementById("substance");
+const behavioralSelect = document.getElementById("behavioral")
+
 if(optionSelected == 1){
-    addictionType = "drug/substance addiction";
+    addictionType = "substance addiction";
     //THE USERS ADDICTION TYPE IS SUBSTANCE
+
+    behavioralSelect.style.display = "none"; //HIDING THE OTHER SELECTOR
 }else{
     addictionType = "behavioral addiction"
     //THE USERS ADDICTION TYPE IS BEHAVIORAL
+
+    substanceSelect.style.display = "none"; //HIDING THE OTHER SELECTOR
 }
+
 
 console.log(optionSelected);
 new TypeIt("#message", {
-    strings: `You selected ${addictionType}`,
+    strings: `You selected the ${addictionType} category`,
     speed: 75,
     waitUntilVisible: true,
     cursor: false
   }).go();
+setTimeout(()=>{
+    document.getElementById("messageTwo").style.display ="block";
+    document.getElementById("main").style.display = "block"; //SHOWING THE MAIN AREA
+}, 4200);
