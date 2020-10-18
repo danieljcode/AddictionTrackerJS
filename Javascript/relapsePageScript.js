dd5b2c0fa7cc711d0cf659efe9285c24a2fb78dc
Daniel John <danieljohncode@tutanota.com>
2020-10-18 18:44:28 +0100

    Almost finished the relapse screen

--- Javascript/relapsePageScript.js
+++ Javascript/relapsePageScript.js
@@ -1,7 +1,18 @@
 const slider = document.getElementById("slider");
 
-slider.addEventListener("change", (event)=>{
+slider.addEventListener("change", (event) => {
 
     console.log(slider.value);
+    //SHOW THE BACK BTN
+    document.getElementById("back").style.display = "block";
+
+    if (slider.value == 5) {
+        console.log("User has set their mood to despairing...");
+        //Show the user advice on low moods...
+
+        document.getElementById("help").style.display = "block"
+    } else {
+        document.getElementById("help").style.display = "none"
+    }
 
 })
